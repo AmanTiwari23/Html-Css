d2fb518fbe217edf4f255dd49825abc8b7062d32
Aman Tiwari <tiwari95aman@gmail.com>
2025-05-21 23:59:07 +0530

    js practice

--- javaScript/lec1.js
+++ javaScript/lec1.js
@@ -1,23 +1,40 @@
-let today = new Date();
-console.log(today);
-let day = today.getDay();
-console.log(day);
-let daylist = [
-  "Sundey",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+// let today = new Date();
+// console.log(today);
+// let day = today.getDay();
+// console.log(day);
+// let daylist = [
+//   "Sundey",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
 
-console.log("today is : " + daylist[day]);
+// console.log("today is : " + daylist[day]);
 
-let hour = today.getHours();
-let min = today.getMinutes();
-let sec = today.getSeconds();
+// let hour = today.getHours();
+// let min = today.getMinutes();
+// let sec = today.getSeconds();
 
-let prepand = (hour >= 12) ? "PM" : "AM";
+// let prepand = (hour >= 12) ? "PM" : "AM";
 
-console.log("current time : " +hour + prepand + " : " + min + " : " + sec)
+// hour = (hour >= 12)? hour -12 :hour ;
+
+// console.log("current time : " +hour + prepand + " : " + min + " : " + sec)
+
+
+
+function animate_string(id) 
+{
+    var element = document.getElementById(id);
+    var textNode = element.childNodes[0]; // assuming no other children
+    var text = textNode.data;
+
+setInterval(function () 
+{
+ text = text[text.length - 1] + text.substring(0, text.length - 1);
+  textNode.data = text;
+}, 100);
+}

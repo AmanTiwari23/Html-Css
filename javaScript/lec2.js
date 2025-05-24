c493f6286747cf032ecb50716c601d96e9b95146
Aman Tiwari <tiwari95aman@gmail.com>
2025-05-24 23:39:37 +0530

    tempracher converter

--- javaScript/lec2.js
+++ javaScript/lec2.js
@@ -1,9 +1,27 @@
-const num = Math.ceil(Math.random()*10);
-console.log(num);
-const guess  = prompt("Guess the number between 1 to 10 ");
+// const num = Math.ceil(Math.random()*10);
+// console.log(num);
+// const guess  = prompt("Guess the number between 1 to 10 ");
 
-if(guess == num){
-    alert("Matched");
-}else{
-    alert("try again");
+// if(guess == num){
+//     alert("Matched");
+// }else{
+//     alert("try again");
+// }
+
+function cToF(celsius) 
+{
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
 }
+
+function fToC(fahrenheit) 
+{
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+} 
+cToF(60);
+fToC(45);

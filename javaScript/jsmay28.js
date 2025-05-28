408f23e484ac8dcd4d41682b0ceb52eb6dfe54a5
Aman Tiwari <tiwari95aman@gmail.com>
2025-05-28 19:27:54 +0530

    json methods

--- javaScript/jsmay28.js
+++ javaScript/jsmay28.js
@@ -1,16 +1,16 @@
-const obj = {name: "aman"};
+const obj = { name: "aman" };
 
-console.log(typeof(obj));
+console.log(typeof obj);
 
 const person = {
-    name: "jonh",
-    age:23,
-    iseligible : true,
-    address: {
-        city: "bhopal",
-        state: "mp",
-    },
-    hobby : ["chess","singing"],
+  name: "jonh",
+  age: 23,
+  iseligible: true,
+  address: {
+    city: "bhopal",
+    state: "mp",
+  },
+  hobby: ["chess", "singing"],
 };
 
 console.log(person.name);
@@ -27,46 +27,56 @@ student.name = "john";
 
 console.log(student);
 
+//array of objects
 
-//array of objects 
-
-const movies=
- [
-    {
-        title:"kgf", relese:2025
-    },
-    {
-        title:"harry Potter", relese: 2010
-    },
-    {
-        title:"stranger things", relese:2020
-    },
-
-]
+const movies = [
+  {
+    title: "kgf",
+    relese: 2025,
+  },
+  {
+    title: "harry Potter",
+    relese: 2010,
+  },
+  {
+    title: "stranger things",
+    relese: 2020,
+  },
+];
 
 console.log(movies[1].title);
 console.log(movies[2].relese);
 
 //spred operator
 
-const obj1 = {a:1, b:2};
-const obj2 = {c:3, d:4};
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
 
-const obj3 = {...obj1,...obj2};
+const obj3 = { ...obj1, ...obj2 };
 
 console.log(obj3);
 
-//de structuring 
+//de structuring
 
 const todo = {
-    id: 1,
-    title: "something",
-    user: "aman",
-}
+  id: 1,
+  title: "something",
+  user: "aman",
+};
 
-const {id:i,title:t, user:u} = todo;
+const { id: i, title: t, user: u } = todo;
 
 console.log(i);
 console.log(t);
 
+const person2 = {
+    name: "jonh",
+    age:23,
+    iseligible : true,
+}
 
+const jsonuser = JSON.stringify(person2);
+console.log(person2);
+console.log(jsonuser);
+
+const backtoobj = JSON.stringify(jsonuser);

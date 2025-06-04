01a1e8fb0d7d9c19b744f556fb5185a3fcb00ca7
Aman Tiwari <tiwari95aman@gmail.com>
2025-06-04 19:25:19 +0530

    dom

---
+++ javaScript/dom.js
@@ -0,0 +1,40 @@
+// replace gi method 
+// hoisting 
+// split
+// splice 
+// 
+
+// const obj = {
+//     name: "aman",
+//     age: 23
+// }
+
+// console.log(obj.name);
+
+// delete obj.age;
+// console.log(obj);
+
+// let str="arvinda"
+// let str1=[...new set(str)].join('');
+// console.log(str1);
+
+
+// let ans = confirm("are you fine");
+
+// console.log(ans);
+
+// let num = 1234;
+// let num2 = num.toString().split("");
+// console.log(num2);
+
+let ptag = document.querySelector("#text");
+
+function func1(){
+    ptag.innerHTML="Hello world";
+}
+
+
+function func2(){
+    alert("I am from function 2");
+}
+

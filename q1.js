206747741a1ada429177a6be5d3af1b4a1e0761e
Aman Tiwari <tiwari95aman@gmail.com>
2025-06-21 20:32:26 +0530

    find greatest in three value

---
+++ q1.js
@@ -0,0 +1,18 @@
+ let a = prompt("enter 1st value");
+  let b = prompt ("enter second value");
+  let c = prompt("enter third value");
+
+  if(a>b){
+    if(a>c){
+      console.log("a is greatest");
+    }else{
+      console.log("c is greatest");
+    }
+  }else if(b>c){
+       
+      console.log("b is greatest");
+
+    }else{
+      console.log("c is gratest");
+    }
+ 

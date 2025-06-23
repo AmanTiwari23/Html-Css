590a7987a2081d2526627314175a72462943565c
Aman Tiwari <tiwari95aman@gmail.com>
2025-06-23 23:41:38 +0530

    array eliment count

--- q1.js
+++ q1.js
@@ -1,18 +1,28 @@
- let a = prompt("enter 1st value");
-  let b = prompt ("enter second value");
-  let c = prompt("enter third value");
+//  let a = prompt("enter 1st value");
+//   let b = prompt ("enter second value");
+//   let c = prompt("enter third value");
 
-  if(a>b){
-    if(a>c){
-      console.log("a is greatest");
-    }else{
-      console.log("c is greatest");
-    }
-  }else if(b>c){
+//   if(a>b){
+//     if(a>c){
+//       console.log("a is greatest");
+//     }else{
+//       console.log("c is greatest");
+//     }
+//   }else if(b>c){
        
-      console.log("b is greatest");
+//       console.log("b is greatest");
 
-    }else{
-      console.log("c is gratest");
-    }
- 
+//     }else{
+//       console.log("c is gratest");
+//     }
+let arr = [2,3,4,2,3,3,4,5,6] 
+
+function countOccurrences(arr) {
+  return arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+}
+
+let arrcount = countOccurrences(arr);
+console.log(arrcount);

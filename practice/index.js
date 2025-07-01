f54de82a3222110662a082b635412dfb958e16e7
Aman Tiwari <tiwari95aman@gmail.com>
2025-07-01 23:52:10 +0530

    practice

--- practice/index.js
+++ practice/index.js
@@ -1,7 +1,12 @@
-let sub = prompt("enter your favoratie subject");
+// let sub = prompt("enter your favoratie subject");
 
-let h4 = document.createElement("h4");
+// let h4 = document.createElement("h4");
 
-h4.innerHTML=sub;
+// h4.innerHTML=sub;
 
-document.getElementsByTagName("body").appendChild(h4);
+// document.getElementsByTagName("body").appendChild(h4);
+const str = "aman@13!!#";
+const remove_non_ASCII = str => str.replace(/[^\x20-\x7E]/g, '');
+
+// Test the function
+console.log(remove_non_ASCII('äÄçÇéÉêw3resouröceÖÐþúÚ')); // Output: w3resource
